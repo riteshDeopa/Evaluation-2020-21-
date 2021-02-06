7a7c1773c16b888f985e74a577427eadc9ad406f
ritesh.deopa <ritesh.deopa@robosoftin.com>
2021-02-06 09:36:35 +0530

    Project Created

---
+++ NewsApp/NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Ritesh Raj Singh Deopa on 06/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
